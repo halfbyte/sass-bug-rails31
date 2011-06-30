0bea9e65b6ae2fe29c14a7fa0bc5396bc9f60bb8
Jan Krutisch <jan@krutisch.de>
2011-07-01 00:57:53 +0200

    sass debug

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails31Template::Application.config.secret_token = '428864ef7ba7301d20e040a2e9fb4aeace0592ac249b8911622d2dfd588f4d233ed2dafa9741831d8823473c668b7a65f89083929bd51c7a7c5dadd97349669c'
